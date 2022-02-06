8266ed31fdb243e171c2ca4d5896d39492e08186
PatrickPurcell <patrick.r.purcell@gmail.com>
2022-02-05 21:05:13 -0800

    Revert "Updating commit hashes"
    This reverts commit c993a5fef74fec12956ea4314140cb8b426b9e62.

--- external/dynamic_static.random.cmake
+++ external/dynamic_static.random.cmake
@@ -1,2 +1,21 @@
-# ==========================================#   Copyright (c) 2021 dynamic_static#       Licensed under the MIT license#     http://opensource.org/licenses/MIT# ==========================================include(FetchContent)if(EXISTS "${DYNAMIC_STATIC}/dynamic_static.random/")    FetchContent_Declare(dynamic_static.random SOURCE_DIR "${DYNAMIC_STATIC}/dynamic_static.random/")else()    FetchContent_Declare(        dynamic_static.random        GIT_REPOSITORY "https://github.com/dynamic-static/dynamic_static.random.git"        GIT_TAG e006edcfe84165c5b7afea148a0d1ad3d8b6b786
-        GIT_PROGRESS TRUE        FETCHCONTENT_UPDATES_DISCONNECTED    )endif()FetchContent_MakeAvailable(dynamic_static.random)
+
+# ==========================================
+#   Copyright (c) 2021 dynamic_static
+#       Licensed under the MIT license
+#     http://opensource.org/licenses/MIT
+# ==========================================
+
+include(FetchContent)
+
+if(EXISTS "${DYNAMIC_STATIC}/dynamic_static.random/")
+    FetchContent_Declare(dynamic_static.random SOURCE_DIR "${DYNAMIC_STATIC}/dynamic_static.random/")
+else()
+    FetchContent_Declare(
+        dynamic_static.random
+        GIT_REPOSITORY "https://github.com/dynamic-static/dynamic_static.random.git"
+        GIT_TAG e3f80c9eede53ddc019373ac34ba793ece2c9aea
+        GIT_PROGRESS TRUE
+        FETCHCONTENT_UPDATES_DISCONNECTED
+    )
+endif()
+FetchContent_MakeAvailable(dynamic_static.random)
